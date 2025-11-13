7c2f4fffe5a71b0eead4a81b001b2a3d84dec67d
Dutga-p <david.camilo.ordonez@correounivalle.edu.co>
2025-11-13 09:38:38 -0500

    updated backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'https://task-manager-production-2275.up.railway.app/api';
+export const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
